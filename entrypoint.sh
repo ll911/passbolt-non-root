be6c4ad56673a58ab7062b94aafafc84ab44ea2d
Leo <leo.lou@gov.bc.ca>
2023-05-23 13:48:00 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 sed -e "s/^www-data:x:[^:]*:[^:]*:/www-data:x:$(id -u):$(id -u):/" /etc/passwd > /tmp/passwd
 cat /tmp/passwd > /etc/passwd
@@ -10,12 +10,8 @@ passbolt_config="/etc/passbolt"
 gpg_private_key="${PASSBOLT_GPG_SERVER_KEY_PRIVATE:-$passbolt_config/gpg/serverkey_private.asc}"
 gpg_public_key="${PASSBOLT_GPG_SERVER_KEY_PUBLIC:-$passbolt_config/gpg/serverkey.asc}"
 
-ssl_key='/etc/passbolt/certs/certificate.key'
-ssl_cert='/etc/passbolt/certs/certificate.crt'
-
-deprecation_message=""
-
-subscription_key_file_paths=("/etc/passbolt/subscription_key.txt" "/etc/passbolt/license")
+ssl_key="/etc/passbolt/certs/certificate.key"
+ssl_cert="/etc/passbolt/certs/certificate.crt"
 
 source /passbolt/entrypoint-rootless.sh
 source /passbolt/entropy.sh
@@ -40,8 +36,6 @@ if [ ! -f "$ssl_key" ] && [ ! -L "$ssl_key" ] && \
 fi
 
 install
-
-echo -e "$deprecation_message"
 
 declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /etc/environment
 
